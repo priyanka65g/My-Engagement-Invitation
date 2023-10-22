ea068611e827442ddcaa85c0566c9af190fedab6
priyanka65g <122663914+priyanka65g@users.noreply.github.com>
2023-10-22 19:07:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var names = prompt("enter your name");
 if(names!=null){
- document.querySelector(".message").textContent="Hiii "+names+", I hope you are eagerly waiting for my marriage"+String.fromCodePoint(0x1FAE3)+". This is the right time to announce our engagement"+String.fromCodePoint(0x148d)+" ceremony. Come and join us to celebrate this new chapter of my life"+String.fromCodePoint(0x1F60B);
+ document.querySelector(".message").textContent="Hiii "+names+", I hope you are eagerly waiting for my marriage"+String.fromCodePoint(0x1F48D)+". This is the right time to announce our engagement"+String.fromCodePoint(0x148d)+" ceremony. Come and join us to celebrate this new chapter of my life"+String.fromCodePoint(0x1F60A);
 }
 else
-document.querySelector(".message").textContent="Hiii, I hope you are eagerly waiting for my marriage"+String.fromCodePoint(0x1FAE3)+". This is the right time to announce our engagement"+String.fromCodePoint(0x1F48d)+" ceremony. Come and join us to celebrate this new chapter of my life"+String.fromCodePoint(0x1F60B);
+document.querySelector(".message").textContent="Hiii, I hope you are eagerly waiting for my marriage"+String.fromCodePoint(0x1F48D)+". This is the right time to announce our engagement"+String.fromCodePoint(0x1F48d)+" ceremony. Come and join us to celebrate this new chapter of my life"+String.fromCodePoint(0x1F60A);
 
